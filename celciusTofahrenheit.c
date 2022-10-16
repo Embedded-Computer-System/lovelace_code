86fa2c230ed4fbea535a02882321e5e1c7ab5e86
Keino.Eliud <elkeino@gmail.com>
2022-10-16 14:13:27 +0300

    added fahrenheit to celius

--- celciusTofahrenheit.c
+++ celciusTofahrenheit.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-    printf("includes \n");
+    int c1= 5;
+    int c2 = 9; 
+	int c3 = 32;
+    float fah = 212.0;
+    float cent = (fah-c3)* c1/c2;
+    printf ("%.2f Fahrenheit are %.2f Centigrades",fah, cent);
     return 0;
 }
